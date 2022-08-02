3f06e2eed8bd8fdc902ce1495e0fb731619578e3
Meghojyoti Pal <meghojp@gmail.com>
2022-08-02 09:44:57 +0530

    Fixing passport strategy and ready to be deployed

--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,45 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
-const users = require('../controllers/users');
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const catchAsync = require('../utils/catchAsync')
+const User = require('../models/user')
+const users = require('../controllers/users')
 
-router.route('/register')
-      .get(users.renderRegister)
-      .post(catchAsync(users.register));
+router
+  .route('/register')
+  .get(users.renderRegister)
+  .post(catchAsync(users.register))
 
-router.route('/login')
-      .get(users.renderLogin)
-      .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);  
+router
+  .route('/login')
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate('local', {
+      failureFlash: true,
+      failureRedirect: '/login',
+    }),
+    users.login
+  )
 
 router.get(
-	'/auth/google',
-	passport.authenticate('google', {
-		scope: ['email']
-	})
-);
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email'],
+  })
+)
 
 router.get(
-	'/auth/google/callback',
-	passport.authenticate('google', {
-            failureFlash: true,
-		failureRedirect: '/login'
-	}),
-	(req, res) => {
-            const redirectUrl = '/campgrounds';
-            res.redirect(redirectUrl);
-      }
-);
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureFlash: true,
+    failureRedirect: '/login',
+  }),
+  (req, res) => {
+    const redirectUrl = '/campgrounds'
+    res.redirect(redirectUrl)
+  }
+)
 
 router.get('/logout', users.logout)
 
-module.exports = router;
+module.exports = router
